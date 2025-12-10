f80740e033b94ec119c6e210a5b2794e8a51666c
Rodrigo Lima <125403873+RgoSL@users.noreply.github.com>
2025-12-10 20:10:37 -0300

    feat(Beginner): add solution for exercise 1006

---
+++ Beginner/1006.py
@@ -0,0 +1,10 @@
+# Variáveis Que Recebem as Notas
+A = float(input())
+B = float(input())
+C = float(input())
+
+# Cálculo da Média Ponderada Das 3 Notas
+Media = ((A * 2) + (B * 3) + (C * 5)) / 10 
+
+# Especificação de 1 Decimal Pós Vírgula
+print(f"MEDIA = {Media:.1f}")
