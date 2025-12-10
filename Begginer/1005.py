7552492b4f4a89ad255c439a5a1692db0f15c1ff
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-12-10 20:08:06 -0300

    chore: rename folder of beginner level

--- Begginer/1005.py
+++
@@ -1,9 +0,0 @@
-# Variáveis Que Recebem Notas
-A = float(input())
-B = float(input())
-
-# Cálculo da Média Ponderada
-Media = ((A * 3.5) + (B * 7.5)) / 11
-
-# Especificação de Decimais Pós Vírgula
-print(f"MEDIA = {Media:.5f}")
---
+++ Beginner/1005.py
@@ -0,0 +1,9 @@
+# Variáveis Que Recebem Notas
+A = float(input())
+B = float(input())
+
+# Cálculo da Média Ponderada
+Media = ((A * 3.5) + (B * 7.5)) / 11
+
+# Especificação de Decimais Pós Vírgula
+print(f"MEDIA = {Media:.5f}")
